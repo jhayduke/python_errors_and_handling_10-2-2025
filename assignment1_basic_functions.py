66d1c3df1212b3158eff1d5afb408ecce29ec84a
jhayduke <joshua.hayduke@gmail.com>
2025-10-02 18:53:36 -0700

    updated assignments

---
+++ assignment1_basic_functions.py
@@ -0,0 +1,37 @@
+
+# ==greeting==
+# def greet(name):
+#   print(f"Hello, {name}!")
+
+# greet("allan")
+
+# ==area==
+# def rectangle_area(length, width):
+#   return length * width
+
+# area = rectangle_area(6, 7)
+# print(area)
+
+
+# def add(num1, num2):
+#   # showing on the screen and then python forgets about it
+#   print(num1 + num2)
+
+# def add_with_return(num1, num2):
+#   # give it back to us to use later in our program
+#   return num1 + num2
+
+# add(5,8)
+
+# result = add_with_return(5, 8)
+# print("The result is:", result)
+# print("The average is:", result/2)
+
+
+# converter
+
+# def celcius_to_farenheit(celcius):
+#   return (celcius * 9/5) + 32
+
+# farenheit = celcius_to_farenheit(23)
+# print("Farenheit:", farenheit)
